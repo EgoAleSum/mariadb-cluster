a94c945fb640468b1c2a77fbaeed10887238af1e
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2016-04-02 14:06:08 -0400

    Generating yaml file

--- sources/cloud-config.source.js
+++ sources/cloud-config.source.js
@@ -2,6 +2,60 @@
 
 var pack = require('./cloud-config.pack.json')
 
+// Source for the cloud-config.yaml file
+var yamlSource = {
+    // JSON structure for the cloud-config.yaml file
+    tree: {
+        "write_files": [],
+        "coreos": {
+            "update": {
+                "reboot-strategy": "etcd-lock"
+            },
+            "etcd2": {
+                "discovery": false,
+                "advertise-client-urls": "http://$private_ipv4:2379,http://$private_ipv4:4001",
+                "initial-advertise-peer-urls": "http://$private_ipv4:2380",
+                "listen-client-urls": "http://0.0.0.0:2379,http://0.0.0.0:4001",
+                "listen-peer-urls": "http://$private_ipv4:2380"
+            },
+            "units": []
+        }
+    },
+
+    // List of files for write_files
+    readFiles: {
+        'docker-mariadb-galera.sh': {
+            'path': '/opt/bin/docker-mariadb-galera.sh',
+            'owner': 'root',
+            'permissions': '0755'
+        },
+        'docker-mariadb-waiter.sh': {
+            'path': '/opt/bin/docker-mariadb-waiter.sh',
+            'owner': 'root',
+            'permissions': '0755'
+        },
+        'etcd-waiter.sh': {
+            'path': '/opt/bin/etcd-waiter.sh',
+            'owner': 'root',
+            'permissions': '0755'
+        },
+        'mysql_server.cnf': {
+            'path': '/opt/mysql.conf.d/mysql_server.cnf',
+            'owner': 'root',
+            'permissions': '0644'
+        }
+    },
+
+    // List of systemd units
+    units: [
+        { 'name': 'docker.service', 'command': 'start' },
+        { 'name': 'etcd2.service', 'command': 'start' },
+        { 'name': 'docker-mariadb-galera.service', 'command': 'start', 'source': 'docker-mariadb-galera.service' },
+        { 'name': 'docker-mariadb-waiter.service', 'command': 'start', 'source': 'docker-mariadb-waiter.service' },
+        { 'name': 'etcd-waiter.service', 'command': 'start', 'source': 'etcd-waiter.service' },
+    ]
+}
+
 // Entry-point for the unit. Generate the cloud-config.yaml file
 var cloudConfig = function(formValues, done) {
     // Check if the etcd2 discovery URL has been passed
@@ -28,8 +82,50 @@ var cloudConfig = function(formValues, done) {
 }
 
 // Build the cloud-config.yaml file
-var buildYaml = function(formValues, done) {
-    console.log(formValues)
+var buildYaml = function(formValues, done) {    
+    // Create the tree
+    var yamlTree = Object.assign({}, yamlSource.tree)
+    
+    // etcd2 discovery url
+    yamlTree.coreos.etcd2.discovery = formValues.discoveryUrl
+    
+    // Read files to be created and append the data to the yaml tree
+    for(var k in yamlSource.readFiles) {
+        if(yamlSource.readFiles.hasOwnProperty(k)) {
+            var push = Object.assign({}, yamlSource.readFiles[k]) // Clone the object
+            push.content = pack[k]
+            yamlTree.write_files.push(push)
+        }
+    }
+    
+    // systemd units
+    for(var i = 0, len = yamlSource.units.length; i < len; i++) {
+        var unit = yamlSource.units[i]
+        
+        var push = Object.assign({}, unit) // Clone the object
+        delete push.source
+        delete push['drop-ins-source']
+        
+        if(unit['drop-ins-source']) {
+            push['drop-ins'] = []
+            for(var k in unit['drop-ins-source']) {
+                if(unit['drop-ins-source'].hasOwnProperty(k)) {
+                    push['drop-ins'].push({
+                        name: k,
+                        content: pack[k]
+                    })
+                }
+            }
+        }
+        if(unit.source) {
+            push.content = pack[unit.source]
+        }
+        
+        yamlTree.coreos.units.push(push)
+    }
+    
+    var yamlString = "#cloud-config\n\n" + jsyaml.safeDump(yamlTree, {lineWidth: -1})
+    console.log(yamlString)
 }
 
 module.exports = cloudConfig
