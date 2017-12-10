5d934744447e2d6e19d20d2fcefb841535fe3c37
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2017-12-09 19:54:40 -0800

    Finally – a button to copy to clipboard

--- sources/app.source.js
+++ sources/app.source.js
@@ -6,32 +6,39 @@ var forms = require('./forms.source.js')
 var cloudConfig = require('./cloud-config.source.js')
 var arm = require('./arm.source.js')
 
+var _clipboard = null
+
 $(document).ready(function() {
     // Hide the output panel
     $('[gen-role="page"][gen-page="output"]').hide()
-    
+
     // Restart button action
     $('[gen-role="restart-button"]').on('click', function() {
         $('[gen-role="page"]').show()
             .filter('[gen-page="output"]').hide()
     })
-    
+
     // Form mode: prepare, and set default to Azure Resource Manager template
     forms.prepareFormMode()
     forms.setFormMode('arm')
-    
+
     // Populate all node sizes in the select and bind action to change event to populate data disk select
     forms.nodeSize()
-    
+
     // Callback to show output
     var showOutput = function(armTemplate, yamlString, yamlB64) {
+        // Restore clipboard.js
+        if(_clipboard) {
+            _clipboard.destroy()
+        }
+
         // Restore button
         $('[gen-role="generate-button"]').prop('disabled', false).text('Generate')
-        
+
         // Show the output panel
         $('[gen-role="page"]').hide()
             .filter('[gen-page="output"]').show()
-        
+
         // Azure Resource Manager template
         if(armTemplate) {
             $('[gen-role="output"][gen-content="arm"]').show()
@@ -40,17 +47,20 @@ $(document).ready(function() {
         else {
             $('[gen-role="output"][gen-content="arm"]').hide()
         }
-        
+
         // cloud-config.yaml
         $('[gen-role="output"][gen-content="yaml"] code').text(yamlString)
         $('[gen-role="output"][gen-content="yamlb64"] code').text(yamlB64)
-        
+
         // Highlight syntax
         $('pre code').each(function(i, block) {
             hljs.highlightBlock(block)
         })
+
+        // Clipboard
+        _clipboard = new Clipboard('.clipboard-btn')
     }
-    
+
     // Bind to form submit action
     forms.formSubmit(function(formValues) {
         // Generate the cloud-config.yaml file
@@ -60,7 +70,7 @@ $(document).ready(function() {
             if(formValues.mode == 'arm') {
                 armTemplate = arm(formValues, yamlB64)
             }
-            
+
             // Show output
             showOutput(armTemplate, yamlString, yamlB64)
         })
